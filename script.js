dfd8c7bc31f66b3e12330d6e2a4243991db27c06
Levi Miller <paullevimiller@gmail.com>
2019-10-08 15:10:46 -0700

    Added final solution

---
+++ script.js
@@ -0,0 +1,22 @@
+// Rick and Morty API: https://rickandmortyapi.com/
+
+// Function that fetches our characters based on the name from the input field
+function fetchCharacters() {
+  const inputValue = document.getElementById('character-input').value;
+
+  fetch(`https://rickandmortyapi.com/api/character/?name=${inputValue}`)
+    .then(resp => resp.json())
+    .then(data => {
+      formatCharacters(data.results);
+    });
+}
+
+// Function that formats the data that has been returned from our character fetch (and appends it to the DOM)
+function formatCharacters(characters) {
+  const charDiv = document.getElementById('characters');
+  charDiv.innerHTML = '';
+
+  characters.forEach(character => {
+    charDiv.innerHTML += `<div><img src="${character.image}"><h2>${character.name}</h2></div>`;
+  });
+}
